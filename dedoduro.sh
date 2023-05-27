f01e75a98a55aec9e2c186e4f12ce9b463773474
Felipe Pereira <felipe.psilveira@outlook.com>
2023-05-27 19:26:34 +0000

    Alterando o dedoduro.sh

--- dedoduro.sh
+++ dedoduro.sh
@@ -15,17 +15,17 @@ processar_bloco() {
         for UltimoOcteto in $(seq 1 255); do
             CONTAGEM=$(curl  --silent "https://otx.alienvault.com/api/v1/indicators/IPv4/${ip}${UltimoOcteto}/general" -H "X-OTX-API-KEY: $KEY" | jq '.pulse_info.count') 2>/dev/null
             ASN=$(cat config.cfg | grep ASN | awk -F "=" '{print $2}')
-	    date=$(date '+%Y-%m-%d__%H:%M:%S')
+            date=$(date '+%Y-%m-%d__%H:%M:%S')
             if [[ $CONTAGEM > 0 ]]; then
                 ContagemTags=$(curl  --silent "https://otx.alienvault.com/api/v1/indicators/IPv4/${ip}${UltimoOcteto}/general" -H "X-OTX-API-KEY: $KEY" | jq -r '.pulse_info.pulses[0].tags | length')
                 ULTIMO_REPORT=$(curl --silent "https://otx.alienvault.com/api/v1/indicators/IPv4/${ip}${UltimoOcteto}/general" -H "X-OTX-API-KEY: $KEY" | jq '.pulse_info.pulses[0].modified')
-         	echo "{\"index\":{\"_index\":\"dedoduro\"}}" > teste_$ip$UltimoOcteto.json
+                echo "{\"index\":{\"_index\":\"dedoduro\"}}" > teste_$ip$UltimoOcteto.json
                 echo "{\"ip\":\"$ip$UltimoOcteto\",\"date\":\"${date}\",\"last_report\":$ULTIMO_REPORT,\"number_report\":\"$ContagemTags\",\"ASN\":\"$ASN\"}" >> teste_$ip$UltimoOcteto.json
                 curl --connect-timeout 950 -m 950 --silent -H 'Content-Type: application/json' -XPOST 'cadeolog.com.br:9200/mina/_bulk?pretty' -u elastic:felipe --data-binary @teste_$ip$UltimoOcteto.json >/dev/null
-	    elif [[ $CONTAGEM == "null" ]]; then
-		ContagemTags=$(curl --retry-all-errors --connect-timeout 2500.0 -m 2500.0 --silent "https://otx.alienvault.com/api/v1/indicators/IPv4/${ip}${UltimoOcteto}/general" -H "X-OTX-API-KEY: $KEY" | jq -r '.pulse_info.pulses[0].tags | length')
+            elif [[ $CONTAGEM == "null" ]]; then
+                ContagemTags=$(curl --retry-all-errors --connect-timeout 2500.0 -m 2500.0 --silent "https://otx.alienvault.com/api/v1/indicators/IPv4/${ip}${UltimoOcteto}/general" -H "X-OTX-API-KEY: $KEY" | jq -r '.pulse_info.pulses[0].tags | length')
                 ULTIMO_REPORT=$(curl --retry-all-errors --connect-timeout 2500.0 -m 2500.0 --silent "https://otx.alienvault.com/api/v1/indicators/IPv4/${ip}${UltimoOcteto}/general" -H "X-OTX-API-KEY: $KEY" | jq '.pulse_info.pulses[0].modified')
-         	echo "{\"index\":{\"_index\":\"dedoduro\"}}" > teste_$ip$UltimoOcteto.json
+                echo "{\"index\":{\"_index\":\"dedoduro\"}}" > teste_$ip$UltimoOcteto.json
                 echo "{\"ip\":\"${ip}${UltimoOcteto}\",\"date\":\"${date}\",\"last_report\":\"${ULTIMO_REPORT}\",\"number_report\":\"${ContagemTags}\",\"ASN\":\"$ASN\"}" >> teste_$ip$UltimoOcteto.json
                 curl --connect-timeout 950 -m 950 --silent -H 'Content-Type: application/json' -XPOST 'cadeolog.com.br:9200/mina/_bulk?pretty' -u elastic:felipe --data-binary @teste_$ip$UltimoOcteto.json >/dev/null
             elif [[ $CONTAGEM == 0 ]]; then
@@ -47,4 +47,4 @@ processar_bloco() {
 
 export -f processar_bloco
 
-printf "%s\n" "${blocos[@]}" | parallel -j "$num_threads" processar_bloco
+echo "$blocos" | xargs -P $num_threads -I {} bash -c 'processar_bloco "$@"' _ {}
